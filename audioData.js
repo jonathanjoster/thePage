ea48da744df7d3832193747555c0656ec6124963
Jonathan <oomiyanaoki@oomiyanaokinoMacBook-ea.local>
2020-11-14 15:32:23 +0900

    commit css and js

---
+++ audioData.js
@@ -0,0 +1,56 @@
+var info = [ // id, imgsrc, title, artist
+    [
+        "aisaika",
+        "url(https://images-fe.ssl-images-amazon.com/images/I/41YG15KYC3L.__AC_QL70_ML2_.jpg)",
+        "愛妻家の朝食",
+        "椎名林檎"
+    ],
+    [
+        "honya",
+        "url(https://2.bp.blogspot.com/-5cvuD0_A_lQ/V_xX12_tWhI/AAAAAAAAAvc/7mcXdGEPShUmyBdPwjFlBvowF-863GXQwCLcB/s200/%25E3%2582%25B9%25E3%2582%25AF%25E3%2583%25AA%25E3%2583%25BC%25E3%2583%25B3%25E3%2582%25B7%25E3%2583%25A7%25E3%2583%2583%25E3%2583%2588%2B2014-11-18%2B19.53.00.png)",
+        "ホニャ",
+        "unmo"
+    ],
+    [
+        "lamer",
+        "url(https://images.unsplash.com/photo-1503942142281-94af0aded523?ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80)",
+        "La Mer",
+        "Claude Debussy"
+    ],
+    [
+        "befine",
+        "url(https://m.media-amazon.com/images/I/71NXo0RYIqL._SS500_.jpg)",
+        "Be Fine",
+        "Madeon"
+    ],
+    [
+        "bell",
+        "url(https://images.pexels.com/photos/225228/pexels-photo-225228.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)",
+        "Belle of the Ball",
+        "Leroy Anderson"
+    ],
+    [
+        "alright",
+        "url(https://livedoor.sp.blogimg.jp/kruchoro-mu/imgs/2/b/2b9ef50e.jpg)",
+        "Be Alright",
+        "Ariana Grande"
+    ],
+    [
+        "yuugi",
+        "url(https://pixabay.com/get/53e5dd424d5bae14f1dc8460962933761437d6e14e50774977287fd49148c0_640.jpg)",
+        "遊戯",
+        "本多俊之"
+    ],
+    [
+        "hime",
+        "url(https://pixabay.com/get/53e5d4404854aa14f1dc8460962933761437d6e14e50774977287fd5974bc5_640.jpg)",
+        "おてんば姫の行進",
+        "すぎやまこういち"
+    ],
+    [
+        "test", 
+        "url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQirO3DIjc08cBFI1zhN8yRYlNegO6AeBT_aw&usqp=CAU)",
+        "DUMMY!!!",
+        "Toby?"
+    ]
+];
---
+++ audioLoad.js
@@ -0,0 +1,9 @@
+var sources = [];
+sources.push(new Audio("src/aisaika.wav"));
+sources.push(new Audio("src/honya.wav"));
+sources.push(new Audio("src/lamer.wav"));
+sources.push(new Audio("src/befine.wav"));
+sources.push(new Audio("src/bell.wav"));
+sources.push(new Audio("src/alright.wav"));
+sources.push(new Audio("src/yuugi.wav"));
+sources.push(new Audio("src/hime.wav"));
